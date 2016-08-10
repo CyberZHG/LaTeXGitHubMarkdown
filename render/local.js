fc9c4a9fc7f9f9be22f89303d27e1d7d71a11be5
CyberZHG <CyberZHG@gmail.com>
2016-08-10 21:57:36 +0800

    Remove render.
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- render/local.js
+++
@@ -1,7 +0,0 @@
-/*jslint browser: true*/
-/*global $, window*/
-window.onmessage = function (event) {
-    'use strict';
-    var data = JSON.parse(event.data);
-    localStorage.setItem(data.key, data.html);
-};
