d9f7dd4c0bd35e99b5756b5d7c2483d78eb9e6f6
CyberZHG <cyberzhg@gmail.com>
2016-08-11 23:20:44 +0800

    OAuth server.

--- server/app/server.js
+++ server/app/server.js
@@ -1,11 +1,27 @@
 'use strict';
 
 const express = require('express');
+const request = require('request');
 
 const app = express();
 
 app.get('/auth', function (req, res) {
-    res.send('Auth');
+    request.post({
+        url: 'https://github.com/login/oauth/access_token',
+        json: true,
+        form: {
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            code: req.query.code
+        }
+    }, function(err, httpResponse, body) {
+        if (err) {
+            res.write(err);
+        } else {
+            res.redirect('https://cyberzhg.github.io/LaTeXGitHubMarkdown/static/access?token=' + body.access_token);
+        }
+        res.end();
+    });
 });
 
 app.listen(80, '0.0.0.0');
