6d1741740824e73e5c84998f028627d666f001fd
CyberZHG <CyberZHG@gmail.com>
2016-08-14 22:16:05 +0800

    Structure.
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- static/raw.js
+++ static/raw.js
@@ -1,65 +1,85 @@
 /*jslint browser: true*/
-/*global $, window, console*/
-$(document).ready(function () {
+/*global $, window, console, require, exports, marked*/
+
+if (typeof require === 'function') {
+    var marked = require('marked');
+}
+
+/**
+ * Escape LaTeX formulas in the raw Markdown text.
+ * @param {string} raw
+ * @return {string}
+ */
+function escape(raw) {
     'use strict';
+    var tokens;
+    marked.setOptions({
+        gfm: true,
+        tables: true,
+        breaks: true
+    });
+    tokens = marked.lexer(raw);
+    console.log(tokens);
+    return raw;
+}
 
-    /**
-     * Get raw Markdown text from the given url in the query string.
-     * @param {function} callback
-     */
-    function getRawContent(callback) {
-        var url = window.getParameterByName('url');
-        if (!url) {
-            url = 'https://raw.githubusercontent.com/CyberZHG/LaTeXGitHubMarkdown/master/README.md';
-        }
-        $.ajax({
-            url: url,
-            success: function (data) {
-                console.log('Get Raw: Success');
-                callback(true, data);
-            },
-            error: function () {
-                console.log('Get Raw: Error');
-                callback(false);
-            }
-        });
-    }
+if (typeof require === 'function') {
+    exports.escape = escape;
+}
 
-    /**
-     * Escape LaTeX formulas in the raw Markdown text.
-     * @param {string} raw
-     * @return {string}
-     */
-    function escape(raw) {
-        return raw;
-    }
+if (typeof require !== 'function') {
+    $(document).ready(function () {
+        'use strict';
 
-    getRawContent(function (success, raw) {
-        if (success) {
-            var url = 'https://api.github.com/markdown',
-                token = localStorage.getItem('latex_github_markdown_access_token'),
-                data = {
-                    'text': escape(raw),
-                    'mode': 'markdown'
-                };
-            if (token) {
-                url += '?access_token=' + token;
-                data.access_token = token;
+        /**
+         * Get raw Markdown text from the given url in the query string.
+         * @param {function} callback
+         */
+        function getRawContent(callback) {
+            var url = window.getParameterByName('url');
+            if (!url) {
+                url = 'https://raw.githubusercontent.com/CyberZHG/LaTeXGitHubMarkdown/master/README.md';
             }
             $.ajax({
                 url: url,
-                type: 'POST',
-                contentType: 'application/json',
-                dataType: 'text',
-                data: JSON.stringify(data),
-                success: function (text) {
-                    console.log('Render: Success');
-                    window.addRenderedToDom(text);
+                success: function (data) {
+                    console.log('Get Raw: Success');
+                    callback(true, data);
                 },
                 error: function () {
-                    console.log('Render: Error');
+                    console.log('Get Raw: Error');
+                    callback(false);
                 }
             });
         }
+
+        getRawContent(function (success, raw) {
+            if (success) {
+                var url = 'https://api.github.com/markdown',
+                    token = localStorage.getItem('latex_github_markdown_access_token'),
+                    data = {
+                        'text': escape(raw),
+                        'mode': 'markdown'
+                    };
+                if (token) {
+                    url += '?access_token=' + token;
+                    data.access_token = token;
+                }
+                $.ajax({
+                    url: url,
+                    type: 'POST',
+                    contentType: 'application/json',
+                    dataType: 'text',
+                    data: JSON.stringify(data),
+                    success: function (text) {
+                        console.log('Render: Success');
+                        window.addRenderedToDom(text);
+                    },
+                    error: function () {
+                        console.log('Render: Error');
+                    }
+                });
+            }
+        });
     });
-});
+}
--- test/test_fake.js
+++ test/test_fake.js
@@ -1,13 +1,15 @@
 /*jslint node: true */
 /*global describe, it */
 'use strict';
-var assert = require('assert');
+var assert = require('assert'),
+    raw = require('../static/raw.js');
 
 var test_time_out = 1000 * 60 * 10;
 
 describe('Fake', function () {
     describe('#fake_describe', function () {
         it('Fake it', function (done) {
+            raw.escape('# Header\nParagraph\n');
             done();
         });
     });
