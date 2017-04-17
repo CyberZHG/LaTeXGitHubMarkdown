d2f839c1e6549d336da10351df953ce6be19cf1e
CyberZHG <CyberZHG@gmail.com>
2017-04-17 20:15:47 +0800

    Receive message.

--- chrome/render.js
+++ chrome/render.js
@@ -58,7 +58,7 @@ function initLaTeX() {
             rendered.src = 'https://cyberzhg.github.io/LaTeXGitHubMarkdown/static/html?key=' + key;
         };
         window.onmessage = function (event) {
-            rendered.style.height = event.data + 'px';
+            rendered.style.height = (parseInt(event.data) + 20) + 'px';
         };
         frame.src = 'https://cyberzhg.github.io/LaTeXGitHubMarkdown/static/local';
     }
